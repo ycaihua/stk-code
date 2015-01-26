7deb2a659b7e120f4d265f6217de487ad36b5a46
Vincent Lejeune <vljn@ovi.com>
2015-01-26 19:16:59 +0100

    Use custom action

---
+++ install/CheckIfDirectoryIsEmpty/CustomAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using System.IO;
+
+
+// From http://stackoverflow.com/questions/17533239/wix-checking-installdir-is-empty
+namespace CheckIfDirectoryIsEmpty
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CheckInstalFolderEmptyness(Session session)
+        {
+            session.Log("CheckDirectory Emptyness");
+
+            string installFolder = session["INSTALLFOLDER"];
+            installFolder = installFolder.Trim();
+            session["InstallDirOk"] = "1";
+            if (Directory.Exists(installFolder) && Directory.GetFiles(installFolder, "*", SearchOption.AllDirectories).Length > 0)
+            {
+                session["InstallDirOk"] = "0";
+            }
+
+            return ActionResult.Success;
+        }
+    }
+}
