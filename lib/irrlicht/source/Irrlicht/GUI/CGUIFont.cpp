7fca87a3298b4c0825c7aa2dd2cc531b55f6f73d
Vincent Lejeune <vljn@ovi.com>
2015-02-06 20:03:26 +0100

    Use an unordered_map for CGUIFont

--- lib/irrlicht/source/Irrlicht/GUI/CGUIFont.cpp
+++ lib/irrlicht/source/Irrlicht/GUI/CGUIFont.cpp
@@ -166,7 +166,7 @@ bool CGUIFont::load(io::IXMLReader* xml)
 				}
 				rectangle.LowerRightCorner.Y = val;
 
-				CharacterMap.insert(ch,Areas.size());
+				CharacterMap.insert(std::make_pair(ch, Areas.size()));
 
 				// make frame
 				f.rectNumber = SpriteBank->getPositions().size();
@@ -364,7 +364,7 @@ void CGUIFont::readPositions(video::IImage* image, s32& lowerRightPositions)
 				Areas.push_back(a);
 				// map letter to character
 				wchar_t ch = (wchar_t)(lowerRightPositions + 32);
-				CharacterMap.set(ch, lowerRightPositions);
+				CharacterMap[ch] = lowerRightPositions;
 
 				++lowerRightPositions;
 			}
@@ -425,9 +425,9 @@ u32 CGUIFont::getSpriteNoFromChar(const wchar_t *c) const
 
 s32 CGUIFont::getAreaFromCharacter(const wchar_t c) const
 {
-	core::map<wchar_t, s32>::Node* n = CharacterMap.find(c);
-	if (n)
-		return n->getValue();
+	std::unordered_map<wchar_t, s32>::const_iterator n = CharacterMap.find(c);
+	if (n != CharacterMap.end())
+		return n->second;
 	else
 		return WrongCharacter;
 }
