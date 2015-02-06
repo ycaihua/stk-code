571bcf6466f0846b97c30c0a8881b392aefd0536
Vincent Lejeune <vljn@ovi.com>
2015-02-07 00:33:26 +0100

    Move IGUITable to GUI/

--- lib/irrlicht/source/Irrlicht/CDefaultGUIElementFactory.cpp
+++ lib/irrlicht/source/Irrlicht/CDefaultGUIElementFactory.cpp
@@ -23,7 +23,7 @@
 #include <GUI/IGUISpinBox.h>
 #include <GUI/IGUIStaticText.h>
 #include <GUI/IGUITabControl.h>
-#include "IGUITable.h"
+#include <GUI/IGUITable.h>
 #include "IGUIToolbar.h"
 #include "IGUIWindow.h"
 #include "IGUITreeView.h"
