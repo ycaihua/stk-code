beb6812731de1bdfec844b620b4dfcf5f06042f1
Vincent Lejeune <vljn@ovi.com>
2015-02-07 16:54:06 +0100

    Additionnal cleanups

--- lib/irrlicht/include/ESceneNodeAnimatorTypes.h
+++ lib/irrlicht/include/ESceneNodeAnimatorTypes.h
@@ -13,33 +13,6 @@ namespace scene
 	//! An enumeration for all types of built-in scene node animators
 	enum ESCENE_NODE_ANIMATOR_TYPE
 	{
-		//! Fly circle scene node animator
-		ESNAT_FLY_CIRCLE = 0,
-
-		//! Fly straight scene node animator
-		ESNAT_FLY_STRAIGHT,
-
-		//! Follow spline scene node animator
-		ESNAT_FOLLOW_SPLINE,
-
-		//! Rotation scene node animator
-		ESNAT_ROTATION,
-
-		//! Texture scene node animator
-		ESNAT_TEXTURE,
-
-		//! Deletion scene node animator
-		ESNAT_DELETION,
-
-		//! Collision respose scene node animator
-		ESNAT_COLLISION_RESPONSE,
-
-		//! FPS camera animator
-		ESNAT_CAMERA_FPS,
-
-		//! Maya camera animator
-		ESNAT_CAMERA_MAYA,
-
 		//! Amount of built-in scene node animators
 		ESNAT_COUNT,
 
